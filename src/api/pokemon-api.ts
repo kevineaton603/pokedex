70a5d90b312624a4f0193cb952aede8492563be9
Kevin Eaton <keaton@polly.co>
2023-10-19 15:35:07 -0400

    fix build

--- src/api/pokemon-api.ts
+++ src/api/pokemon-api.ts
@@ -1,11 +1,4 @@
-import {
-  Pokemon as PokemonType,
-  Ability,
-  EvolutionChain,
-  EvolutionTrigger,
-  NamedAPIResource,
-  PokemonSpecies,
-} from "../models";
+import { PokeAPI } from "pokeapi-types";
 
 const BASE_URI = "https://pokeapi.co/api/v2";
 
@@ -14,41 +7,41 @@ export const getPokemons = async (limit = 1000, offset = 0) => {
     `${BASE_URI}/pokemon?limit=${limit}&offset=${offset}`
   );
   const json = await response.json();
-  return json.results as Array<NamedAPIResource>;
+  return json.results as Array<PokeAPI.NamedAPIResource>;
 };
 
 export const getPokemon = async (value: string | number) => {
   const response = await fetch(`${BASE_URI}/pokemon/${value}`);
   const json = await response.json();
-  return json as PokemonType;
+  return json as PokeAPI.Pokemon;
 };
 
 export const getManyPokemon = async (values: Array<string | number>) => {
   const requests = values.map((value) => getPokemon(value));
   const responses = await Promise.all(requests);
-  return responses as Array<PokemonType>;
+  return responses as Array<PokeAPI.Pokemon>;
 };
 
 export const getAbility = async (value: string | number) => {
   const response = await fetch(`${BASE_URI}/ability/${value}`);
   const json = await response.json();
-  return json as Ability;
+  return json as PokeAPI.Ability;
 };
 
 export const getSpecies = async (value: string | number) => {
   const response = await fetch(`${BASE_URI}/pokemon-species/${value}`);
   const json = await response.json();
-  return json as PokemonSpecies;
+  return json as PokeAPI.PokemonSpecies;
 };
 
 export const getEvolution = async (value: string | number) => {
   const response = await fetch(`${BASE_URI}/evolution-chain/${value}`);
   const json = await response.json();
-  return json as EvolutionChain;
+  return json as PokeAPI.EvolutionChain;
 };
 
 export const getEvolutionTrigger = async (value: string | number) => {
   const response = await fetch(`${BASE_URI}/evolution-trigger/${value}`);
   const json = await response.json();
-  return json as EvolutionTrigger;
+  return json as PokeAPI.EvolutionTriggers;
 };
